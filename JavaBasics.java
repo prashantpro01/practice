ea1d635eba5b3d1db39cfd5c684825339afcbea7
Prashant <ppy.ppy.1997@gmail.com>
2023-03-18 06:02:34 +0530

    hollow rectangle

--- JavaBasics.java
+++ JavaBasics.java
@@ -231,6 +231,27 @@ public class JavaBasics {
             }
             System.out.println();
         }
+    return;
+    }
+
+    // hollow rohmbus
+    public static void hollow_rohmbus(int n){
+        for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1 ; j<=n; j++ ){
+                if(i == 1 || i == n || j == 1 || j == n ){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+        System.out.println( );
+        }
+        return;
     }
     public static void main (String[] args) {
         // Scanner sc =new Scanner(System.in);
@@ -238,7 +259,8 @@ public class JavaBasics {
         // double n2 = sc.nextDouble();
         // double n3 = sc.nextDouble();
         
-        solid_rohmbus(5);
+        hollow_rohmbus(5);
+        //solid_rohmbus(5);
         //butterfly(5);
         //binTri(5);
         
