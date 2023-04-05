505fde8c16aa98475b482f9e6746b48cabcae778
prashantpro01 <ppy.ppy.1997@gmail.com>
2023-04-05 09:45:07 +0530

    insersion sort

--- BasicSorting.java
+++ BasicSorting.java
@@ -35,9 +35,24 @@ public static void selectionSort(int arr[]){
             System.out.print(arr[i]+" ");
         }
     }
+    public static void insertionSort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int curr =arr[i];
+            int prev=i-1;
+            //finding out the current position to insert
+            while(prev >=0 && arr[prev]>curr){
+               arr[prev+1]=arr[prev];
+               prev--; 
+            }
+            //inseting
+            arr[prev+1]=curr;
+        }
+    }
     public static void main(String args[]){
         int arr[]={5,4,1,3,2}; 
-        selectionSort(arr);
+        insertionSort(arr);
+        
+        //selectionSort(arr);
     
         // bubbleSort(arr);
         printArr(arr);
