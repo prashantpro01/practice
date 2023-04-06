c65b477ee7d576bc3b7f91199f6fbf2a8132ea23
prashantpro01 <ppy.ppy.1997@gmail.com>
2023-04-06 08:39:14 +0530

    counting sort works in shorter number stack

--- BasicSorting.java
+++ BasicSorting.java
@@ -49,10 +49,28 @@ public static void selectionSort(int arr[]){
             arr[prev+1]=curr;
         }
     }
+    public static void countingSort(int arr[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            largest=Math.max(largest, arr[i]);
+        }
+        int count[]=new int[largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        //sorting
+        int j=0;
+        for(int i=0; i<count.length;i++){
+            while(count[i]>0){
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }}
+    }
     public static void main(String args[]){
-        int arr[]={5,4,1,3,2}; 
-        
-        Arrays.sort(arr,Collection.reverseOrder());//only works on object example (Integer) 
+        int arr[]={5,7,7,1,4,1,3,2}; 
+        countingSort(arr);
+        // Arrays.sort(arr,Collection.reverseOrder());//only works on object example (Integer) 
 
         //Arrays.sort(arr, 0, 3);
 
