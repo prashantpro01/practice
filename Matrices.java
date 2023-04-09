825b355f6044fdaf98a030ad8c58c2ea6fb9be6b
Prashant <ppy.ppy.1997@gmail.com>
2023-04-09 21:20:17 +0530

    spiral matrix print concept

--- Matrices.java
+++ Matrices.java
@@ -1,41 +1,48 @@
 import java.util.*;
 
 public class Matrices {
-    public static boolean search(int matrix[][],int key){
-        for(int i=0;i<matrix.length;i++){
-    for(int j=0;j<matrix[0].length;j++){
-    if(matrix[i][j]==key){
-        System.out.println("found at"+i+","+j);
-        return true;}
-}
-}System.out.println("not found");
-return false;
-    }
-    public static void largest(int matrix[][]){
-        int largest=Integer.MIN_VALUE;
-        for(int i=0;i<matrix.length;i++){
-    for(int j=0;j<matrix[0].length;j++){
-    if(matrix[i][j]>largest){
-    largest=matrix[i][j];}
-}
-}
-System.out.println(largest);
-    }
-    public static void main(String args[]){
-    int matrix[][]=new int[3][3];
-    int n=matrix.length, m=matrix[0].length;
-    Scanner sc=new Scanner(System.in);
-    for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-    matrix[i][j]=sc.nextInt();
-}}
-for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-    System.out.print( matrix[i][j]+" ");
-    
-}System.out.println();}
-search(matrix,5);
-largest(matrix);
+    public static boolean search(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("found at" + i + "," + j);
+                    return true;
+                }
+            }
+        }
+        System.out.println("not found");
+        return false;
     }
 
+    public static void largest(int matrix[][]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] > largest) {
+                    largest = matrix[i][j];
+                }
+            }
+        }
+        System.out.println(largest);
+    }
+
+    public static void main(String args[]) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+        search(matrix, 5);
+
+    }
 }
