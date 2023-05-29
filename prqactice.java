3b2b417aeca2282a359a1c0cd14b089cf0c47972
prashantpro01 <ppy.ppy.1997@gmail.com>
2023-05-29 20:09:12 +0530

    practice

--- prqactice.java
+++ prqactice.java
@@ -172,16 +172,42 @@ public class prqactice {
     }
 
     public static void hollodiamond(int n){
+        int m=(n-1)/2;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i==1 || i==n || j==1 || j==n){
-                    System.out.print("*");
-                }else if(j!=1||j!=n){
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+            System.out.print("*");
         }
+        System.out.println( );
+        for(int i=1;i<=m;i++){
+            for(int j=m-i;j>=0;j--){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=m-i;j>=0;j--){
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
+        for(int i=m-1;i>=1;i--){
+            for(int j=m-i;j>=0;j--){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=m-i;j>=0;j--){
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
+        for(int i=1;i<=n;i++){
+            System.out.print("*");
+        }
+        
+        
+       
+        
     }
 
     public static void main(String args[]){
