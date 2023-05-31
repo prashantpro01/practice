ff5d81f0e1834c32cda6aa40685fac23226863af
prashantpro01 <ppy.ppy.1997@gmail.com>
2023-05-31 18:07:49 +0530

    practice

--- prqactice.java
+++ prqactice.java
@@ -285,10 +285,87 @@ public class prqactice {
          
     }
 
-    public static void q6D2(int n){
-        
+    
+
+    public static void q1D3(int n){
+        n=(n+1)/2;
+        for(int i=1;i<n;i++){
+            for(int j=(n*2)-(2*i);j>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(int j=i;j>=2;j--){
+                System.out.print(" ");  
+            }
+            for(int j=i;j>=3;j--){
+                System.out.print(" ");   
+            }
+            for(int j=1;j<=i ;j++){
+                if(i==1){
+
+                }else{
+                    System.out.print(j);
+                }
+            }    
+
+            System.out.println();
+
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=(n*2)-(2*i);j>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(int j=i;j>=2;j--){
+                System.out.print(" ");  
+            }
+            for(int j=i;j>=3;j--){
+                System.out.print(" ");   
+            }
+            for(int j=1;j<=i ;j++){
+                if(i==1){
+
+                }else{
+                    System.out.print(j);
+                }
+            }    
+
+            System.out.println();
+
+        }
     }
 
+    public static void q2D3(int n){
+        for(int i=n;i>0;i--){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void q3D3(int n){
+        int x=1;
+        for(int i=0;i<=n-1;i++){
+            for(int j=n-i-1;j>0;j--){
+                System.out.print("*");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print(x);
+                x++;
+            }
+            
+            System.out.println();
+        }
+    }
 
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
@@ -300,7 +377,9 @@ public class prqactice {
         // q2D2(n);
         //  q3D2(n);
     //    q4D2(n);
-    q5D2(n);
-
+    // q5D2(n);
+        // q1D3(n);
+        // q2D3(n);
+        q3D3(n);
     }
 }
