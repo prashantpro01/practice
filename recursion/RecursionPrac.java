785eaef37155fe6d829b137702080829b425a359
Prashant <ppy.ppy.1997@gmail.com>
2024-03-24 12:04:29 +0530

    Merge branch 'master' of https://github.com/prashantpro01/practice

--- recursion/RecursionPrac.java
+++ recursion/RecursionPrac.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class RecursionPrac {
     public static void printDec(int n) {
         if (n == 1) {
@@ -36,18 +37,51 @@ public class RecursionPrac {
         return sum;
     }
 
-    public static int fib(int n){
-        if(n==0||n==1){
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
             return n;
         }
-        int fnm1=fib(n-1);
-        int fnm2=fib(n-2);
-        return fnm1+fnm2;
+        int fnm1 = fib(n - 1);
+        int fnm2 = fib(n - 2);
+        return fnm1 + fnm2;
     }
+
+    public static int tilingProb(int n) {
+        // base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        // vetical
+        int fnm1 = tilingProb(n - 1);
+        // horizontal
+        int fnm2 = tilingProb(n - 2);
+
+        int toways = fnm1 + fnm2;
+        return toways;
+    }
+
+    public static void removeDuplicate(String str, int idx, StringBuilder newStr, boolean map[]) {
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (map[currChar - 'a'] == true) {
+            removeDuplicate(str, idx + 1, newStr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            removeDuplicate(str, idx + 1, newStr.append(currChar), map);
+        }
+    }
+
     public static void main(String[] args) {
-        //  printDec(10);
-        //  printInc(5);
-        System.out.print(fib(10));
+        // printDec(10);
+        // printInc(5);
+        // System.out.print(fib(10));
+        // System.out.println(tilingProb(5));
+        String str = "aaabbccffggddjjkklleerrmmnnbbkkiiyyjj";
+        removeDuplicate(str, 0, new StringBuilder(""), new boolean[26]);
+        
         
     }
 }
